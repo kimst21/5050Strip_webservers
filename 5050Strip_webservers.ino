89da253ec1382da54dd51fbefd5474a3ddcab0a3
kimst21 <118581137+kimst21@users.noreply.github.com>
2024-03-06 20:45:43 +0900

    Update 5050Strip_webservers.ino

--- 5050Strip_webservers.ino
+++ 5050Strip_webservers.ino
@@ -1,275 +1,140 @@
-#include <ArduinoJson.h>
-
 #include <WiFi.h>
-#include <WiFiClient.h>
-#include <WebServer.h>
-#include <ESPmDNS.h>
-
-#include "wifisecrets.h"
-
-// LED 핀의 개수
-const int blueLedPin = 2;
-const int redLedPin = 0;
-const int greenLedPin = 4;
-
-// PWM 속성 설정
-const int freq = 5000;
-const int blueLedChannel = 10;
-const int redLedChannel = 11;
-const int greenLedChannel = 12;
-const int resolution = 8;
-
-// 불필요하게 변경되지 않도록 현재 값을 추적합니다
-byte currentRed = 255;
-byte currentGreen = 255;
-byte currentBlue = 255;
-
-#define MAXIMUM_PATTERN_COMPONENTS 20
 
 // 여기 와이파이 자격 증명...
-const char* ssid = "**********";
-const char* password = "**********";
+const char* ssid     = "--------";
+const char* password = "--------";
 
-// HTTP에서 실행 중인 웹 서버
-WebServer server(80);
+// 웹 서버 포트 번호를 80으로 설정
+WiFiServer server(80);
 
-// 특정 시간에 색상을 지정하기 위한 클래스(이들은 보간됨)
-class ColorAndTime
-{
-public:
-  // 빨간색, 녹색 및 파란색 값
-  byte red;
-  byte green;
-  byte blue;
-  // 시간(마이크로초)
-  unsigned long time;
+// HTTP GET 값 디코딩
+String redString = "0";
+String greenString = "0";
+String blueString = "0";
+int pos1 = 0;
+int pos2 = 0;
+int pos3 = 0;
+int pos4 = 0;
 
-public:
-  // 생성할 생성자(배열 초기화에 사용 가능)
-  ColorAndTime(byte newRed, byte newGreen, byte newBlue, unsigned long newTime)
-  {
-    red = newRed;
-    green = newGreen;
-    blue = newBlue;
-    time = newTime;
-  }
+// HTTP 요청을 저장할 변수
+String header;
 
-  //초기화자 없이 배열을 지정할 때 사용되는 기본 빈 생성자
-  ColorAndTime()
-  {
-    red = 0;
-    green = 0;
-    blue = 0;
-    time = 0;
-  }
+// PWM제어를 위한 빨강, 초록, 파랑 핀
+const int redPin =   45;     //  GPIO45
+const int greenPin = 46;     //  GPIO46
+const int bluePin =  48;     //  GPIO49
 
-};
+// PWM 주파수, 채널 및 비트 분해능 설정
+const int freq = 5000;
+const int redChannel = 0;
+const int greenChannel = 1;
+const int blueChannel = 2;
+// 비트 해상도 256
+const int resolution = 8;
 
-// 패턴 배열
-ColorAndTime pattern[MAXIMUM_PATTERN_COMPONENTS];
-size_t statesInPattern;
+// 현재시간
+unsigned long currentTime = millis();
+// 이전 시간
+unsigned long previousTime = 0; 
+// 제한 시간(밀리초)을 정의합니다(example: 2000ms = 2s)
+const long timeoutTime = 2000;
 
-
-bool connectToWifi(const char* ssid, const char* password) {
-  int waitTime = 0;
-
-  WiFi.mode(WIFI_STA);
+void setup() {
+  Serial.begin(115200);
+  // LED PWM 기능 라이트 구성
+  ledcSetup(redChannel, freq, resolution);
+  ledcSetup(greenChannel, freq, resolution);
+  ledcSetup(blueChannel, freq, resolution);
+  
+  // 채널을 제어할 GPIO에 연결합니다
+  ledcAttachPin(redPin, redChannel);
+  ledcAttachPin(greenPin, greenChannel);
+  ledcAttachPin(bluePin, blueChannel);
+  
+  // SSID와 비밀번호로 Wi-Fi 네트워크에 연결
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
   WiFi.begin(ssid, password);
-  Serial.println("");
-
-  // 연결 대기
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
-    waitTime++;
-    if (waitTime > 20)
-    {
-      WiFi.disconnect();
-      return false;
-    }
-
   }
+  // 로컬 IP 주소를 인쇄하고 웹 서버를 시작합니다
   Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-
-  if (MDNS.begin("esp32")) {
-    Serial.println("MDNS responder started");
-  }
-
-  return true;
-}
-
-void handleRoot() {
-  server.send(200, "text/plain", "hello from esp8266!");
-}
-
-void handlePattern() {
-  Serial.println("In POST /pattern");
-  Serial.println(server.args());
-  Serial.println(server.argName(0));
-  Serial.println(server.arg(0));
-
-  String body = server.arg("plain");
-
-  DynamicJsonDocument doc(ESP.getMaxAllocHeap());  
-  auto error = deserializeJson(doc, body);
-
-  if (error) {
-    Serial.print(F("deserializeJson() failed with code "));
-    Serial.println(error.c_str());
-    server.send(400);
-    return;
-  }
-
-  Serial.println("Deserialized JSON doc");
-
-  JsonArray patternArray = doc.as<JsonArray>();
-  if (!patternArray) {
-    server.send(400, "text/plain", "Top level JSON object should be an array of pattern values");
-    return;
-  }
-
-  if ((patternArray.size() < 2) || (patternArray.size() > MAXIMUM_PATTERN_COMPONENTS)) {
-    // 작업: MAXIM_PATTN_COMPONTENS(스프린트)를 올바르게 보고하려면 이 오류 메시지를 가져옵니다
-    server.send(400, "text/plain", "Number of pattern elements should be between 2 and 20");
-    return;
-  }
-
-  for (size_t i = 0; i < patternArray.size(); i++)
-  {
-    // 작업: 개체에서 이러한 구성 요소를 확인합니다
-    // 배열의 개체에서 구성 요소를 꺼냅니다
-    int r = patternArray[i]["r"];
-    int g = patternArray[i]["g"];
-    int b = patternArray[i]["b"];
-    int t = patternArray[i]["t"];
-
-    pattern[i].red = r;
-    pattern[i].green = g;
-    pattern[i].blue = b;
-    pattern[i].time = t;
-  }
-
-  // 어레이에 실제로 있는 구성 요소의 수를 추적하도록 글로벌 설정
-  statesInPattern = patternArray.size();
-
-  server.send(200);
-}
-
-
-void initializeDefaultPattern()
-{
-  // "검은색"에서 "무지개"를 거쳐 "검은색"으로 되돌아오는 패턴을 8초에 걸쳐 만듭니다
-  pattern[0] = {0, 0, 0, 0};
-  pattern[1] = {0, 0, 0, 1000000L};
-  pattern[2] = {255, 0, 0, 2000000L};
-  pattern[3] = {255, 255, 0, 3000000L};
-  pattern[4] = {0, 255, 0, 4000000L};
-  pattern[5] = {0, 255, 255, 5000000L};
-  pattern[6] = {0, 0, 255, 6000000L};
-  pattern[7] = {255, 0, 255, 7000000L};
-  pattern[8] = {0, 0, 0, 8000000L};
-
-  statesInPattern = 9;
-}
-
-void setup()
-{
-  // 디버깅 중에 출력을 확인해야 할 경우를 대비하여 직렬을 시작합니다.
-  Serial.begin(115200);
-
-  //이것이 약간 신뢰할 수 없기 때문에 여러 번 연결해 보십시오
-  bool connected = false;
-  while (!connected)
-  {
-    connected = connectToWifi(ssid, password);
-  }
-
-  // 채널 설정
-  ledcSetup(redLedChannel, freq, resolution);
-  ledcSetup(greenLedChannel, freq, resolution);
-  ledcSetup(blueLedChannel, freq, resolution);
-
-  // 채널에 핀을 부착합니다
-  ledcAttachPin(redLedPin, redLedChannel);
-  ledcAttachPin(greenLedPin, greenLedChannel);
-  ledcAttachPin(blueLedPin, blueLedChannel);
-
-  //초기 상태 작성
-  ledcWrite(redLedChannel, currentRed);
-  ledcWrite(greenLedChannel, currentGreen);
-  ledcWrite(blueLedChannel, currentBlue);
-
-  // 기본 패턴으로 패턴 채우기
-  initializeDefaultPattern();
-
-  // 결국 테스트를 위해 REST API를 호출하려면 Javacapt가 포함된 페이지여야 합니다
-  server.on("/", handleRoot);
-  // 이 페이지는 패턴을 업데이트하기 위해 RESTful JSON POST를 처리합니다
-  server.on("/pattern", HTTP_POST, handlePattern);
-
-  // 서버를 시작합니다
   server.begin();
 }
 
-void updateColorChannel(byte& currentValue, byte newValue, int channel)
-{
-  //새 값이 현재 값과 다를 경우에만 업데이트합시다
-  if (currentValue != newValue)
-  {
-    // 현재 값 업데이트
-    currentValue = newValue;
-    ledcWrite(channel, newValue);
-  }
-}
+void loop(){
+  WiFiClient client = server.available();   // 들어오는 클라이언트 듣기
 
-void updateColorsWithPattern(unsigned long currentTime, bool invertValues)
-{
-  //우리가 어느 브래킷에 있는지 파악합니다(어느 두 패턴 섹션 사이에 있는지)
-  size_t colorBracketStart = 0;
-  for (size_t index = 0; index < (statesInPattern - 1); index++)
-  {
-    if (currentTime >= (pattern[index].time) && (currentTime < pattern[index + 1].time))
-    {
-      colorBracketStart = index;
-      break;
+  if (client) {                             // 새로운 클라이언트가 연결되면,
+    currentTime = millis();
+    previousTime = currentTime;
+    Serial.println("New Client.");          // 메시지를 시리얼 포트에 인쇄합니다
+    String currentLine = "";                // 클라이언트에서 들어오는 데이터를 유지하기 위한 문자열 만들기
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {            // 클라이언트가 연결되어 있는 동안 루프
+      currentTime = millis();
+      if (client.available()) {             // 고객으로부터 읽을 바이트가 있다면,
+        char c = client.read();             // 바이트를 읽습니다
+        Serial.write(c);                    // 시리얼 모니터를 출력합니다
+        header += c;
+        if (c == '\n') { // 바이트가 새 줄 문자인 경우,현재 줄이 비어 있으면 두 줄의 새 줄 문자가 연속으로 표시됩니다
+                         // 클라이언트 HTTP 요청은 여기까지이므로 응답을 보냅니다:
+          if (currentLine.length() == 0) {
+            // HTTP 헤더는 항상 응답 코드(예: HTTP/1.1200 OK)로 시작합니다
+            // 그리고 콘텐츠 유형을 지정하여 고객이 무엇이 올지 알 수 있도록 하고 빈 줄로 표시합니다:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+                   
+            // HTML 웹 페이지 표시
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            client.println("<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\">");
+            client.println("<script src=\"https://cdnjs.cloudflare.com/ajax/libs/jscolor/2.0.4/jscolor.min.js\"></script>");
+            client.println("</head><body><div class=\"container\"><div class=\"row\"><h1>ESP Color Picker</h1></div>");
+            client.println("<a class=\"btn btn-primary btn-lg\" href=\"#\" id=\"change_color\" role=\"button\">Change Color</a> ");
+            client.println("<input class=\"jscolor {onFineChange:'update(this)'}\" id=\"rgb\"></div>");
+            client.println("<script>function update(picker) {document.getElementById('rgb').innerHTML = Math.round(picker.rgb[0]) + ', ' +  Math.round(picker.rgb[1]) + ', ' + Math.round(picker.rgb[2]);");
+            client.println("document.getElementById(\"change_color\").href=\"?r\" + Math.round(picker.rgb[0]) + \"g\" +  Math.round(picker.rgb[1]) + \"b\" + Math.round(picker.rgb[2]) + \"&\";}</script></body></html>");
+            // HTTP 응답은 다른 빈 줄로 끝납니다
+            client.println();
+
+            // 요청 샘플 : /?r201g32b255&
+            // 레드 = 201 | 그린 = 32 | 블루 = 255
+            if(header.indexOf("GET /?r") >= 0) {
+              pos1 = header.indexOf('r');
+              pos2 = header.indexOf('g');
+              pos3 = header.indexOf('b');
+              pos4 = header.indexOf('&');
+              redString = header.substring(pos1+1, pos2);
+              greenString = header.substring(pos2+1, pos3);
+              blueString = header.substring(pos3+1, pos4);
+              
+              ledcWrite(redChannel, redString.toInt());
+              ledcWrite(greenChannel, greenString.toInt());
+              ledcWrite(blueChannel, blueString.toInt());
+            }
+            // 도중에 루프를 해제합니다
+            break;
+          } else { // 새 줄이 있으면 현재 줄을 지웁니다
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // 만약 당신이 마차 복귀 캐릭터 외에 다른 것이 있다면,
+          currentLine += c;      // 현재 라인의 끝에 추가합니다
+        }
+      }
     }
+    // 헤더 변수 지우기
+    header = "";
+    // 연결 닫기
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
   }
-
-  // 브래킷의 길이와 브래킷의 거리를 파악합니다
-  float bracketLength = float(pattern[colorBracketStart + 1].time - pattern[colorBracketStart].time);
-  float timeIntoBracket = float(currentTime - pattern[colorBracketStart].time);
-
-  // 브래킷의 각 끝에 있는 색상의 선형 보간 가중치를 구합니다
-  float percentageColorA = 1.0 - (timeIntoBracket / bracketLength);
-  float percentageColorB = 1.0 - percentageColorA;
-
-  byte redValue = byte(percentageColorA * float(pattern[colorBracketStart].red) + percentageColorB * float(pattern[colorBracketStart + 1].red));
-  byte greenValue = byte(percentageColorA * float(pattern[colorBracketStart].green) + percentageColorB * float(pattern[colorBracketStart + 1].green));
-  byte blueValue = byte(percentageColorA * float(pattern[colorBracketStart].blue) + percentageColorB * float(pattern[colorBracketStart + 1].blue));
-
-  // 값을 반전시킬 필요가 있는지 확인합니다(공통 애노드 대 캐소드에 적합)
-  if (invertValues) {
-    redValue = 255 - redValue;
-    greenValue = 255 - greenValue;
-    blueValue = 255 - blueValue;
-  }
-
-  //잠재적으로 채널 업데이트
-  updateColorChannel(currentRed, redValue, redLedChannel);
-  updateColorChannel(currentGreen, greenValue, greenLedChannel);
-  updateColorChannel(currentBlue, blueValue, blueLedChannel);
-}
-
-void loop()
-{
-  // 색상 업데이트
-  unsigned long currentTime = micros() % pattern[statesInPattern - 1].time;
-  updateColorsWithPattern(currentTime, false);
-
-  // REST 활동 점검
-  server.handleClient();
 }
